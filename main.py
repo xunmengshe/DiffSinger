3222e092d9b5bcabd875e42c6a3ec1f6c3d241e8
IceKyrin <40847087+IceKyrin@users.noreply.github.com>
2022-11-05 10:07:24 +0800

    对接svc，预处理直接加载mel、f0；infer增加升降调功能

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 import numpy as np
 import torch
 
-from infer_utils import cross_fade, trans_key
+from inference.infer_utils import cross_fade, trans_key
 from inference.ds_cascade import DiffSingerCascadeInfer
 from inference.ds_e2e import DiffSingerE2EInfer
 from utils.audio import save_wav
