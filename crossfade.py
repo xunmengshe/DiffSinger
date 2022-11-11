e0dc542ca353d80c71a9e15360efd014aca01a0b
yqzhishen <yangqian_1015@icloud.com>
2022-11-11 13:53:22 +0800

    Fix crossfade bug

--- crossfade.py
+++ crossfade.py
@@ -6,6 +6,6 @@ def cross_fade(a: np.ndarray, b: np.ndarray, idx: int):
     fade_len = a.shape[0] - idx
     np.copyto(result[:idx], a[:idx])
     k = np.linspace(0, 1.0, num=fade_len, endpoint=True)
-    result[idx: a.shape[0]] = k * a[idx:] + (1 - k) * b[: fade_len]
+    result[idx: a.shape[0]] = (1 - k) * a[idx:] + k * b[: fade_len]
     np.copyto(b[fade_len:], result[a.shape[0]:])
     return result
