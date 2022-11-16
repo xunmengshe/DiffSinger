346131742f27fd7265c26e81b603b019225f1bb1
yqzhishen <yangqian_1015@icloud.com>
2022-11-17 00:19:36 +0800

    Support coverage checking, oov detection and distribution summary

--- data_gen/singing.py
+++ data_gen/singing.py
@@ -1,9 +1,12 @@
 import glob
+import os.path
 import re
 
-from basics.base_binarizer import BaseBinarizer, BASE_ITEM_ATTRIBUTES
-from utils.phoneme_utils import build_phoneme_list
+import matplotlib.pyplot as plt
 
+from basics.base_binarizer import BaseBinarizer, BASE_ITEM_ATTRIBUTES
+from utils.hparams import hparams
+from utils.phoneme_utils import build_phoneme_list
 
 SINGING_ITEM_ATTRIBUTES = BASE_ITEM_ATTRIBUTES + ['f0_fn']
 
@@ -44,7 +47,7 @@ class SingingBinarizer(BaseBinarizer):
             item['txt'] = open(f'{piece_path.replace(wav_suffix, txt_suffix)}', encoding='utf-8').readline()
             item['ph'] = open(f'{piece_path.replace(wav_suffix, ph_suffix)}', encoding='utf-8').readline()
             item['wav_fn'] = piece_path
-            item['spk_id'] = re.split('-|#', piece_path.split('/')[-2])[0]
+            item['spk_id'] = re.split('[-#]', piece_path.split('/')[-2])[0]
             item['tg_fn'] = piece_path.replace(wav_suffix, tg_suffix)
             item_name = piece_path[len(processed_data_dir)+1:].replace('/', '-')[:-len(wav_suffix)]
             if len(self.processed_data_dirs) > 1:
@@ -53,6 +56,43 @@ class SingingBinarizer(BaseBinarizer):
             
             self.items[item_name] = item
 
+    def generate_summary(self, phone_set: set):
+        # Group by phonemes.
+        phoneme_map = {}
+        for ph in sorted(phone_set):
+            phoneme_map[ph] = 0
+        for item in self.items.values():
+            for ph, slur in zip(item['ph'].split(), item['is_slur']):
+                if ph not in phone_set or slur == 1:
+                    continue
+                phoneme_map[ph] += 1
+        # Draw graph.
+        plt.rcParams['axes.unicode_minus'] = False
+        plt.figure(figsize=(int(len(phone_set) * 0.8), 10))
+        x = list(phoneme_map.keys())
+        values = list(phoneme_map.values())
+        plt.bar(x=x, height=values)
+        plt.tick_params(labelsize=15)
+        plt.xlim(-1, len(phone_set))
+        for a, b in zip(x, values):
+            plt.text(a, b + 2, b, ha='center', va='bottom', fontsize=15)
+        plt.grid()
+        plt.title('Phoneme Distribution Summary', fontsize=30)
+        plt.xlabel('Phoneme', fontsize=20)
+        plt.ylabel('Number of occurrences', fontsize=20)
+        plt.savefig(fname=os.path.join(hparams['binary_data_dir'], 'phoneme_distribution.jpg'),
+                    bbox_inches='tight',
+                    pad_inches=0.25)
+        print('===== Phoneme Distribution Summary =====')
+        for i, key in enumerate(sorted(phoneme_map.keys())):
+            if i == len(phone_set) - 1:
+                end = '\n'
+            elif i % 15 == 14:
+                end = ',\n'
+            else:
+                end = ', '
+            print(f'\'{key}\': {phoneme_map[key]}', end=end)
+
     def load_ph_set(self, ph_set):
         # load those phones that appear in the actual data
         for item in self.items.values():
@@ -60,12 +100,13 @@ class SingingBinarizer(BaseBinarizer):
         # check unrecognizable or missing phones
         actual_phone_set = set(ph_set)
         required_phone_set = set(build_phoneme_list())
+        self.generate_summary(required_phone_set)
         if actual_phone_set != required_phone_set:
             unrecognizable_phones = actual_phone_set.difference(required_phone_set)
             missing_phones = required_phone_set.difference(actual_phone_set)
-            assert False, 'transcriptions and dictionary mismatch:\n' \
-                          f' + {sorted(unrecognizable_phones)}' \
-                          f' - {sorted(missing_phones)}'
+            raise AssertionError('transcriptions and dictionary mismatch.\n'
+                                 f' (+) {sorted(unrecognizable_phones)}\n'
+                                 f' (-) {sorted(missing_phones)}')
 
 
 if __name__ == "__main__":
